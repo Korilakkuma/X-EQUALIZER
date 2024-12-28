7a680453212e5d4f340f308de15e058b9d4e135f
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-12-28 20:23:14 +0900

    feat: Increase frequency band

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 (() => {
-  const FREQUENCIES = [125, 250, 500, 1000, 2000, 4000, 8000];
+  const FREQUENCIES = [62.5, 125, 250, 500, 1000, 2000, 4000, 8000, 16000];
 
   const PRESETS = {
-    flat: [0, 0, 0, 0, 0, 0],
-    perfect: [9, 7, 6, 5, 7, 9],
-    explosion: [9, 7, 6, 5, 7, 4],
-    acoustic: [5, 1, 2, 3, 4, 2],
-    bassbooster: [3, 2, 1, 0, 0, 0],
-    bassreducer: [-4, -3, -1, 0, 0, 0],
-    deep: [2, 1, 3, 2, 1, -2],
-    hiphop: [2, 3, -2, -2, 2, -1],
-    latin: [0, 0, -2, -2, -2, 0],
-    loudness: [0, 0, -2, 0, -1, -5],
-    lounge: [-1, -2, 4, 2, 0, -2],
-    piano: [0, 2, 3, 1, 3, 4],
-    rb: [6, 2, -3, -2, 2, 3],
+    flat         : [0, 0, 0, 0, 0, 0, 0, 0],
+    perfect      : [9, 7, 6, 5, 7, 9],
+    explosion    : [9, 7, 6, 5, 7, 4],
+    acoustic     : [5, 1, 2, 3, 4, 2],
+    bassbooster  : [3, 2, 1, 0, 0, 0],
+    bassreducer  : [-4, -3, -1, 0, 0, 0],
+    deep         : [2, 1, 3, 2, 1, -2],
+    hiphop       : [2, 3, -2, -2, 2, -1],
+    latin        : [0, 0, -2, -2, -2, 0],
+    loudness     : [0, 0, -2, 0, -1, -5],
+    lounge       : [-1, -2, 4, 2, 0, -2],
+    piano        : [0, 2, 3, 1, 3, 4],
+    rb           : [6, 2, -3, -2, 2, 3],
     treblebooster: [0, 0, 0, 1, 3, 4],
     treblereducer: [0, 0, 0, -2, -3, -4]
   };
@@ -32,7 +32,7 @@
         this.filters[i] = this.context.createBiquadFilter();
         this.filters[i].type = 'peaking';
         this.filters[i].frequency.value = FREQUENCIES[i];
-        this.filters[i].Q.value = 2;
+        this.filters[i].Q.value = Math.SQRT1_2;
         this.filters[i].gain.value = 0;
       }
 
@@ -65,13 +65,15 @@
     /** @override */
     params() {
       return {
-        125: this.filters[0].gain.value,
-        250: this.filters[1].gain.value,
-        500: this.filters[2].gain.value,
-        1000: this.filters[3].gain.value,
-        2000: this.filters[4].gain.value,
-        4000: this.filters[5].gain.value,
-        8000: this.filters[6].gain.value
+        62   : this.filters[0].gain.value,
+        125  : this.filters[1].gain.value,
+        250  : this.filters[2].gain.value,
+        500  : this.filters[3].gain.value,
+        1000 : this.filters[4].gain.value,
+        2000 : this.filters[5].gain.value,
+        4000 : this.filters[6].gain.value,
+        8000 : this.filters[7].gain.value,
+        16000: this.filters[8].gain.value
       };
     }
   }
